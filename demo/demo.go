ecd3a72d5894d891fa1ecde5b68638f9c3a6cf6f
faker <1158213354@qq.com>
2019-05-03 13:42:16 +0800

    fd

--- demo/demo.go
+++ demo/demo.go
@@ -1,58 +1,30 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
+
+func reserve(set []int) {
+	for i, j := 0, len(set)-1; i < j; i, j = i+1, j-1 {
+		set[i], set[j] = set[j], set[i]
+	}
+}
 
 func main() {
-	var N int = 1
-	var s string
-	var m1 = [...]string{"jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"}
-	var m2 = [...]string{"tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"}
+	var N, K, input int
 	fmt.Scan(&N)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		s = input.Text()
-		t, err := strconv.Atoi(s)
-		if err != nil {
-			L := strings.Split(s, " ")
-			n := 0
-			for i := 0; i < len(L); i++ {
-				j := 0
-				for j = 0; j < len(m1); j++ {
-					if m1[j] == L[i] {
-						n += j + 1
-					}
-				}
-				for j = 0; j < len(m2); j++ {
-					if m2[j] == L[i] {
-						n += (j + 1) * 13
-					}
-				}
-			}
-			fmt.Println(n)
-		} else {
-			if t >= 13 {
-				t1 := t / 13
-				t %= 13
-				fmt.Printf("%s", m2[t1-1])
-				if t != 0 {
-					fmt.Printf(" %s\n", m1[t-1])
-				} else {
-					fmt.Printf("\n")
-				}
-			} else {
-				if t != 0 {
-					fmt.Printf("%s\n", m1[t-1])
-				} else {
-					fmt.Printf("tret\n")
-				}
-			}
-		}
+	fmt.Scan(&K)
+	var set []int
+	for i := 0; i < N; i++ {
+		fmt.Scan(&input)
+		set = append(set, input)
 	}
-
+	K %= N
+	reserve(set[N-K:])
+	reserve(set[:N-K])
+	reserve(set)
+	for i := 0; i < len(set); i++ {
+		if i > 0 {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("%d", set[i])
+	}
 }
